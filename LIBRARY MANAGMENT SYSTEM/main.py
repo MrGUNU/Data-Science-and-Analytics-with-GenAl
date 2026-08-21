089253483c1413db384bc80aa25bde7cff3b2648
MrGUNU <145864129+MrGUNU@users.noreply.github.com>
2026-08-21 19:33:16 +0530

    feat: initialize library management system with JSON storage and update OOP practice notebook

---
+++ LIBRARY MANAGMENT SYSTEM/main.py
@@ -0,0 +1,65 @@
+import json
+import random
+import string
+from pathlib import Path
+from datetime import datetime
+
+class Library:
+    database = "library.json"
+    data = {"books": [], "members": []}
+
+    # Load existing data to Json
+
+    if Path(database).exists():
+        with open(database, "r") as f:
+            content = f.read().strip()
+            if content:
+                data = json.loads(content)
+    else:
+        with open(database, "w") as f:
+            json.dump(data)
+
+    
+    def gen_id(Prefix = "B"):
+        random_id = ""
+        for i in range(5):
+            random_id += random.choice(string.ascii_uppercase + string.digits)
+
+        return Prefix + "-" + random_id
+
+    def add_book(self):
+        title = input("Enter the book title: ")
+        author = input("Enter the author of the book: ")
+        copies = int(input("How many copies: "))
+
+
+        book = {
+            "id" : Library.gen_id(),
+            "title" : title,
+            "author" : author,
+            "total_copies" : copies,
+            "available_copies" : copies,
+            "added_on" : datetime.now().strftime("%Y-%m-%d %H-%M-%S") 
+        }
+
+        print(book)
+
+obj = Library()
+
+
+print("="*50)
+print("Library Management System")
+print("="*50)
+print("1. Add Book")
+print("2. List Books")
+print("3. Add Members")
+print("4. List Members")
+print("5. Borrow Book")
+print("6. Return Book")
+print("0. Exit the Portal")
+print("-"*50)
+
+choice = input("What do you want to do: ")
+
+if choice == "1":
+    obj.add_book()
