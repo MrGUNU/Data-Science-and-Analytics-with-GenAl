744332e8ca93ffbf336d0b1c94083f5b174685ba
MrGUNU <145864129+MrGUNU@users.noreply.github.com>
2026-08-22 22:35:48 +0530

    feat: implement Streamlit-based library management system with JSON data persistence

--- LIBRARY MANAGMENT SYSTEM/main.py
+++ LIBRARY MANAGMENT SYSTEM/main.py
@@ -71,37 +71,114 @@ class Library:
             "borrowed": []
         }
 
+        Library.data['members'].append(members)
+        Library.save_data()
+        print("Member added sucessfully")
+
     def list_members(self):
         if not Library.data['members']:
             print("There are no members")
         for m in Library.data['members']:
-            print(f"SSSSS")
+            print(f"{m['id']:12} {m['name'][:24]:25} {m['email'][:29]:30}")
+            print("This guy has currently")
+            print(f"{m['borrowed']}")
+        print()
+
+    def borrow(self):
+        member_id = input("Enter the member ID: ").strip()
+        members = [m for m in Library.data['members'] if m['id'] == member_id]
+        if not members:
+            print("No such ID exist")
+            return
+        members = members[0]
+
+        book_id = input("Enter the book ID: ")
+        books = [b for b in Library.data['books'] if b['id'] == book_id]
+
+        if not books:
+            print("Sorry no such id of book exist")
+            return
+        book = books[0]
+
+        if book['available_copies'] <= 0:
+            print("Sorry no books available")
+            return
+
+        borrow_entry = {
+            "book_id": book['id'],
+            "title": book['title'],
+            "borrow_on": datetime.now().strftime("%Y-%m-%d %H-%M-%S"),
+        }
+
+        members['borrowed'].append(borrow_entry)
+        book['available_copies'] -= 1
+        Library.save_data()
+
+
+    def return_book(self):
+        member_id = input("Enter the mermber ID : ").strip()
+        members = [m for m in Library.data['members'] if m['id'] == member_id]
+        if not members:
+            print("no such Id exist")
+            return 
+        
+        member = members[0]
+
+        if not member['borrowed']:
+            print("no borrowed books")
+            return 
+        
+        print("borrowed books")
+        for i, b in enumerate(member['borrowed'],start = 1):
+            print(f"{i}. {b['title']} ({b['book_id']})")
+        
+        try:
+            choice = int(input("enter number to return : - "))
+            selected = member['borrowed'].pop(choice - 1)
+        except Exception as err:
+            print("invalid value ")
+        
+        books = [bk for bk in Library.data['books'] if bk['id'] == selected['book_id'] ]
+        if books:
+            books[0]['available_copies'] += 1
+        
+        Library.save_data()
 
 obj = Library()
 
+while True:
+        
+    print("="*50)
+    print("Library Management System")
+    print("="*50)
+    print("1. Add Book")
+    print("2. List Books")
+    print("3. Add Members")
+    print("4. List Members")
+    print("5. Borrow Book")
+    print("6. Return Book")
+    print("0. Exit the Portal")
+    print("-"*50)
 
-print("="*50)
-print("Library Management System")
-print("="*50)
-print("1. Add Book")
-print("2. List Books")
-print("3. Add Members")
-print("4. List Members")
-print("5. Borrow Book")
-print("6. Return Book")
-print("0. Exit the Portal")
-print("-"*50)
+    choice = input("What do you want to do: ")
 
-choice = input("What do you want to do: ")
+    if choice == "1":
+        obj.add_book()
 
-if choice == "1":
-    obj.add_book()
+    if choice == "2":
+        obj.list_books()
 
-if choice == "2":
-    obj.list_books()
+    if choice == "3":
+        obj.add_members()
 
-if choice == "3":
-    obj.add_members()
+    if choice == "4":
+        obj.list_members()
 
-if choice == "4":
-    obj.list_members()
+    if choice == "5":
+        obj.borrow()
+
+    if choice == "6":
+        obj.return_book()
+
+    if choice == "0":
+        exit(0)
